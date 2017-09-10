b9fe751f74eaa538a1c5a2a6c1055c11677715c6
Kausik Dey <kausik008@gmail.com>
2017-09-10 22:16:59 +0530

    added ip verifier

--- api/v1/request.php
+++ api/v1/request.php
@@ -7,7 +7,7 @@ class Request{
     private $reqUrl = "";
 
     public function __construct($url){
-        $this->auth = $this->getAuthTokens();
+        $this->authToken = $this->getAuthToken();
         $this->reqUrl = $url;
     }
 
@@ -19,7 +19,7 @@ class Request{
             
             // Set some options - we are passing in a useragent too here
             curl_setopt_array($curl, array(
-                CURLOPT_HTTPHEADER => array("authorization:".$this->auth->token->authorization),
+                CURLOPT_HTTPHEADER => array("authorization:".$this->authToken),
                 CURLOPT_RETURNTRANSFER => 1,
                 CURLOPT_URL => $this->reqUrl
             ));
@@ -39,8 +39,36 @@ class Request{
         return $resp; 
     }
 
-    private function getAuthTokens() {
-        return json_decode(file_get_contents("./../auth/authTokens.json"));
+    private function getAuthToken() {
+        //set ip as global variable and fetch public ip address for auth tokens
+        $GLOBALS["ip"] = isset($GLOBALS["ip"]) ? $GLOBALS["ip"] : $this->getIpAddr();
+        $authTokens = json_decode(file_get_contents("./../auth/authTokens.json"),true);
+        if(!isset($authTokens[$GLOBALS["ip"]])) {
+           die('Message: ' ."Error no auth token found for IP: ".$GLOBALS["ip"]);
+        }
+        return $authTokens[$GLOBALS["ip"]]["token"]["authorization"];
+    }
+
+    private function getIpAddr()
+    {
+        // Get cURL resource
+        $curl = curl_init();
+        
+        // Set some options - we are passing in a useragent too here
+        curl_setopt_array($curl, array(
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => "http://freegeoip.net/json/"
+        ));
+
+        // Send the request & save response to $resp
+        $resp = curl_exec($curl);
+        if(!$resp ){
+            die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
+        }
+        // Close request to clear up some resources
+        curl_close($curl);
+
+        return json_decode($resp)->ip;
     }
 }
 ?>
